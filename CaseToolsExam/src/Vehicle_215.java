8ef80633396adfb6a897a31e68ef9da4e22b82ed
admin <admin@Bcalab3-m10>
2022-06-15 09:06:34 +0530

    commitVehicle

---
+++ CaseToolsExam/src/Vehicle_215.java
@@ -0,0 +1,19 @@
+
+public class Vehicle_215 {
+
+	String brand;
+	int speed;
+	
+	Vehicle_215(String brand)
+	{
+		this.brand=brand;
+		this.speed=0;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
